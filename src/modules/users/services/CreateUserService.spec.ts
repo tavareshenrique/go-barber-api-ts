1e2b9300f0a34c1ea05a110bd003f6e28210df6a
Henrique Tavares <ihenrits@gmail.com>
2020-05-06 06:30:50 -0300

    Testando criaçao de usuario

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createAppointment = new CreateUserService(fakeUsersRepository);
+
+    const user = await createAppointment.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createAppointment = new CreateUserService(fakeUsersRepository);
+
+    await createAppointment.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createAppointment.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
