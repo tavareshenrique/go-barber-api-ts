d881d7b16c2dca2db2b1bb3b7442550e7fe3b6e2
Henrique Tavares <ihenrits@gmail.com>
2020-05-09 18:48:05 -0300

    Rota e Controller de Perfil

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -37,6 +37,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntre@example.com');
   });
 
+  it('should not be able show the profile from non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change the email to another existing email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
