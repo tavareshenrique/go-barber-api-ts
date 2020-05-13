86f90377ed0beb0c3af6cc951ca7866b39efbced
Henrique Tavares <ihenrits@gmail.com>
2020-05-12 23:16:05 -0300

    Validando dados

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,7 +4,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 import cors from 'cors';
-
+import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 
@@ -19,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
