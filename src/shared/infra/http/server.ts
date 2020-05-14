7a3dc322ceab506e1a0bb02984d49bd31819d65e
Henrique Tavares <ihenrits@gmail.com>
2020-05-14 07:53:46 -0300

    Express Rate Limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 
 import routes from './routes';
 
@@ -16,6 +17,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
