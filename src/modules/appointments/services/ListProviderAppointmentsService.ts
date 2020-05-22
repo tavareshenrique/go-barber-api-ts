bc18691c5bc56e60ad45059a49afe27fe6b9e414
Henrique Tavares <ihenrits@gmail.com>
2020-05-22 06:49:08 -0300

    Clientes dos agendamentos

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -29,9 +29,11 @@ class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointment[]> {
     const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    let appointments = await this.cacheProvider.recover<Appointment[]>(
-      cacheKey,
-    );
+    // let appointments = await this.cacheProvider.recover<Appointment[]>(
+    //   cacheKey,
+    // );
+
+    let appointments = '';
 
     if (!appointments) {
       appointments = await this.appointmentsRepository.findAllInDayFromProvider(
